050a951aafaeafe856543ac39c5ac8b7057fed1e
EvgrafovMichail <autumnevilmen@gmail.com>
2025-01-20 14:21:23 +0300

    Add TensorConfig model

---
+++ src/tritonazer/entities/tensor_config.pyi
@@ -0,0 +1,25 @@
+from collections.abc import Sequence
+from typing import overload
+
+from .enumerations import TritonDataTypes
+
+class TritonTensorConfig:
+    name: str
+    data_type: TritonDataTypes
+    dims: list[int]
+
+    @overload
+    def __init__(
+        self,
+        name: str,
+        data_type: TritonDataTypes,
+        dims: Sequence[int],
+    ) -> None: ...
+    @overload
+    def __init__(
+        self,
+        name: str,
+        data_types: str,
+        dims: Sequence[int],
+    ) -> None: ...
+    def copy(self, deep: bool = True) -> TritonTensorConfig: ...
