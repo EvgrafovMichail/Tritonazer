dc3ee94fedeb28dad644450e38677f5167134785
EvgrafovMichail <autumnevilmen@gmail.com>
2025-01-28 23:17:05 +0300

    Add TritonModelConfig pyi-file

---
+++ src/tritonazer/entities/model_config.pyi
@@ -0,0 +1,64 @@
+from collections.abc import Iterable, Iterator
+from typing import overload
+
+from .tensor_config import TritonTensorConfig
+
+class TritonModelConfig:
+    path: str
+    name: str
+    max_batch_size: int
+    platfrom: str
+    inputs: Iterator[TritonTensorConfig]
+    outputs: Iterator[TritonTensorConfig]
+
+    @overload
+    def __init__(
+        self,
+        path: str,
+        name: str,
+        inputs: TritonTensorConfig,
+        outputs: TritonTensorConfig,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self,
+        path: str,
+        name: str,
+        inputs: Iterable[TritonTensorConfig],
+        outputs: Iterable[TritonTensorConfig],
+    ) -> None: ...
+    @overload
+    def __init__(
+        self,
+        path: str,
+        name: str,
+        inputs: TritonTensorConfig,
+        outputs: TritonTensorConfig,
+        max_batch_size: int,
+    ) -> None: ...
+    @overload
+    def __init__(
+        self,
+        path: str,
+        name: str,
+        inputs: Iterable[TritonTensorConfig],
+        outputs: Iterable[TritonTensorConfig],
+        max_batch_size: int,
+    ) -> None: ...
+    @overload
+    def add_inputs(self, inputs: TritonTensorConfig) -> None: ...
+    @overload
+    def add_inputs(self, inputs: Iterable[TritonTensorConfig]) -> None: ...
+    @overload
+    def add_outputs(self, outputs: TritonTensorConfig) -> None: ...
+    @overload
+    def add_outputs(self, outputs: Iterable[TritonTensorConfig]) -> None: ...
+    @overload
+    def remove_inputs(self, inputs: TritonTensorConfig) -> None: ...
+    @overload
+    def remove_inputs(self, inputs: Iterable[TritonTensorConfig]) -> None: ...
+    @overload
+    def remove_outputs(self, outputs: TritonTensorConfig) -> None: ...
+    @overload
+    def remove_outputs(self, outputs: Iterable[TritonTensorConfig]) -> None: ...
+    def copy(self) -> TritonModelConfig: ...
